c6a6913c78f1e603f57d8eea7a0a3cb5169476f1
Osiel781 <36452775+Osiel781@users.noreply.github.com>
2018-05-17 13:33:12 +0200

    Add files via upload

---
+++ tests/conexion.php
@@ -0,0 +1,10 @@
+<?php
+
+//Conexion a la base de datos
+	$server="localhost";
+	$username="root";
+	$password="";
+	$db='caruma';
+	$con=mysqli_connect("127.0.0.1", "root", "root", "caruma");
+
+?>
